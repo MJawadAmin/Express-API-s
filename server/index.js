6b24fd0911b376c32059326506d03c401e75e876
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-21 15:09:25 +0500

    server connected

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const UserModel = require("./models/User");
 
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+const PORT = process.env.PORT;
 const dbURI = process.env.dbURI;
 
 mongoose.connect(dbURI)
@@ -18,22 +19,25 @@ mongoose.connect(dbURI)
     .catch(err => console.error("Database connection error:", err));
 
 app.post('/createUser', async (req , res )=>{
-    try {
+    try{
         const {name , email , age }= req.body;
         const User = new UserModel({
-            name: name ,
+            name : name ,
             email : email ,
-            age : age
+            age : age 
         })
-        await User.save();
+        await User.save()
         res.status(200).json({
-            status: "created",
-            message : "success"
+            status :'connected ',
+            message : ' success'
         })
-    } catch(err){
+    }catch (err){
         res.status(200).json({
-            status: "Failed ",
-            messsage : "Server issue "
+            status : 'failed ',
+            message : 'Server Issue '
         })
     }
+})
+app.listen(3000 , ()=>{
+    console.log(`Server Connected Succesfully at ${PORT}`)
 })
